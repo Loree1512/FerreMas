93948a38cc130d6dd6ae5ced40878e89a3cbac54
Seba <sebastianmartin.sg@gmail.com>
2025-05-16 17:52:06 -0400

    testing registro usuarios

--- tienda/urls.py
+++ tienda/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('productos', views.lista_productos, name='lista_productos'),
     path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
+     path('registro/', views.register_view, name='registro'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Producto
 # Create your views here.
 
@@ -13,3 +15,25 @@ def lista_productos(request):
 def detalle_producto(request, producto_id):
     producto = get_object_or_404(Producto, pk=producto_id)
     return render(request, 'tienda/detalle_producto.html', {'producto': producto})
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'El nombre de usuario ya está en uso.')
+            return redirect('registro')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'El correo ya está registrado.')
+            return redirect('registro')
+
+        # Esta función guarda el password de forma segura
+        User.objects.create_user(username=username, email=email, password=password)
+
+        messages.success(request, 'Usuario registrado correctamente.')
+        return redirect('inicio')  # O redirige donde tú quieras
+
+    return render(request, 'tienda/registro.html')
