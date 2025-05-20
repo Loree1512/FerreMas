ddcf0e312bff819f150e7a0ad7b472b278a58ae6
Seba <sebastianmartin.sg@gmail.com>
2025-05-20 12:11:19 -0400

    checkout y ordenes

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Orden, ItemOrden
 # Register your models here.
 
 
 admin.site.register(Producto)
+class ItemOrdenInline(admin.TabularInline):
+    model = ItemOrden
+    extra = 0
+
+class OrdenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre_cliente', 'email', 'total', 'fecha')
+    inlines = [ItemOrdenInline]
+
+admin.site.register(Orden, OrdenAdmin)
--- tienda/models.py
+++ tienda/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=200)
@@ -9,3 +10,21 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Orden(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    nombre_cliente = models.CharField(max_length=100)
+    email = models.EmailField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Orden #{self.id} - {self.nombre_cliente}'
+    
+class ItemOrden(models.Model):
+    orden = models.ForeignKey(Orden, related_name='items', on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.producto.nombre} x {self.cantidad}'
